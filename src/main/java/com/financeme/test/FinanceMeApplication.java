2e9e28fbcede5d93db76b672ad3af0ac1f483872
Palash <parpanipalash@gmail.com>
2023-06-04 21:47:05 +0530

    .

--- src/main/java/com/financeme/test/FinanceMeApplication.java
+++ src/main/java/com/financeme/test/FinanceMeApplication.java
@@ -16,23 +16,24 @@ public class FinanceMeApplication {
     public static void main( String[] args ) throws InterruptedException, IOException
     
     {
-    	//System.setProperty("webdriver.chrome.driver", "D:\\Project\\Banking\\Banking_Selenium\\selenium\\chromedriver.exe");
+    	//System.setProperty("webdriver.chrome.driver", "D:\\Project\\Banking\\test\\test\\chromedriver.exe");
     	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
     	ChromeOptions chromeoptions = new ChromeOptions();
-    	chromeoptions.addArguments("--headless=new");
+    	//chromeoptions.addArguments("--headless=new");
     	chromeoptions.addArguments("--remote-allow-origins=*");
     	WebDriver driver = new ChromeDriver(chromeoptions);
     	
     	System.out.println("Selenium test script has started...");
     	
     	driver.get("http://10.0.128.6:8081/contact.html");
+    	//driver.get("http://localhost:8080/contact.html");
     	Thread.sleep(2000);
     	driver.findElement(By.name("Name")).sendKeys("Palash");
     	driver.findElement(By.name("Phone Number")).sendKeys("82732819");
     	driver.findElement(By.name("Email")).sendKeys("Palash@gmail.com");
     	driver.findElement(By.name("Message")).sendKeys("I would like to open an account..");
     	
-    	driver.findElement(By.className("send_bt")).click();
+    	driver.findElement(By.xpath("//div[@class='send_bt']//a[1]")).click();
     	
     	String successMessage = driver.findElement(By.id("message")).getText();
     	String expectedMessage = "Email Sent";
@@ -43,7 +44,7 @@ public class FinanceMeApplication {
     	
     	File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
     	
-    	//File destFile = new File ("D:\\Project\\Banking\\Banking_Selenium\\selenium\\artifact.png");
+    	//File destFile = new File ("D:\\Project\\Banking\\test\\test\\artifact.png");
     	File destFile = new File ("/home/ubuntu/test-artifact/artifact.png");
     	FileUtils.copyFile(srcFile, destFile);
     	
