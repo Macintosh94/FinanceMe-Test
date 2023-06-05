c4ca9a8c7821329f591c55689ff037b3e759a0cf
Macintosh94 <125908834+Macintosh94@users.noreply.github.com>
2023-06-05 14:34:37 +0530

    Update FinanceMeApplication.java

--- src/main/java/com/financeme/test/FinanceMeApplication.java
+++ src/main/java/com/financeme/test/FinanceMeApplication.java
@@ -28,7 +28,7 @@ public class FinanceMeApplication {
     	System.out.println("Selenium test script has started...");
     	
     	//driver.get("http://10.0.128.6:8081/contact.html");
-    	driver.get("http://localhost:8081/contact.html");
+    	driver.get("http://10.0.128.6:8081/contact.html");
     	Thread.sleep(2000);
     	driver.findElement(By.name("Name")).sendKeys("Palash");
     	driver.findElement(By.name("Phone Number")).sendKeys("82732819");
