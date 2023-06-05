2623a6d9bf99cd8e65c221c24e8316924ec8b942
Palash <palash@email.com>
2023-06-05 09:26:14 +0000

    Adding the Terraform Config and prod-server-deployment.yml

--- Prod-Server_Infra.tf
+++ Prod-Server_Infra.tf
@@ -7,74 +7,47 @@ terraform{
          }
 }
 
-# Configure Providers
-
+locals {
+  security_group_id = "sg-03c8373331e81fb6a"
+  vpc_id = "vpc-047b174833ca44699"
+  subnet_id = "subnet-0701a949f6a48fe16"
+  internet_gateway_id = "igw-03bec895e5bb2ffbb"
+}
 provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIATVOIMA34DCNGVROL"
-  secret_key = "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"
+  region = "us-east-1"
+  shared_config_files      = ["/var/lib/jenkins/workspace/test-server-deployment/config"]
+  shared_credentials_files = ["/var/lib/jenkins/workspace/test-server-deployment/credentials"]
+}
+
+resource "aws_network_interface" "prod-ni" {
+   subnet_id  = local.subnet_id
+   private_ips = ["10.0.128.7"]
+   security_groups = [local.security_group_id]
 
 }
 
-# Deploy Ec2 instances
+resource "aws_eip" "prod-eip" {
+  vpc  = true
+  network_interface = aws_network_interface.test-ni1.id
+  associate_with_private_ip = "10.0.128.7"
 
-resource "aws_instance" "prod-server"{
-        ami = "ami-053b0d53c279acc90"
-        key_name = "AWS_Key"
-        instance_type = "t2.micro"
-        tags = {
-        Name = "Prod-Server"
-        }
+}
 
-resource "null_resource" "localinventorynull02" {
-         triggers = {
-                 mytest = timestamp()
-         }
-        
-         provisioner "local-exec" {
-             command = "echo ${aws_instance.prod-server.tags.Name} ansible_host=${aws_instance.prod-server.public_ip} ansible_user=ec2-user>> inventory"
+resource "aws_instance" "prod-server" {
+  ami                         = "ami-053b0d53c279acc90" #us-east-1
+  instance_type               = "t2.micro"
+  key_name                    = "AWS_Key"
+  availability_zone  = "us-east-1a"
 
-           }
 
- 
-         depends_on = [
+  network_interface {
+  network_interface_id = aws_network_interface.prod-ni.id
+  device_index = 0
+  } 
 
-                          null_resource.localinventorynull01
-                          ]
-          }
-resource "null_resource" "mydynamicinventory" {
+  tags = { 
+    Name = "Prod-Server"
+  }
 
-        triggers = {
-                mytest = timestamp()
-        }
+}
 
-        provisioner "local-exec" {
-            command = "scp inventory ubuntu@54.89.174.78:/tmp/"
-  
-          }
-        depends_on = [
-                       
-                        null_resource.localinventorynull02 , null_resource.localinventorynull01
-                        ]
-          }
-resource  "null_resource"  "ssh3" {
-
-	triggers = {
-		mytest = timestamp()
-	}
-
-	connection {
-	    type     = "ssh"
-	    user     = "ubuntu"
-	    private_key = "/home/ubuntu/AWS_Key.pem"
-	    host     = var.ansible
-	  }
-
-	provisioner "remote-exec" {
-	    inline = [
-              "sudo chmod 777 /tmp/inventory",
-              "sudo mv /tmp/inventory /root/ansible/inventory",
-	    ]
-	  }
-                        
-        
